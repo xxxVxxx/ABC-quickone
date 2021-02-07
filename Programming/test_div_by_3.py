bc8adb089e96b08a3a0051d035fe3b17c6c6bd96
Madhav <maddy4chat@gmail.com>
2021-02-08 08:47:40 +1100

    adding .gitignore and test file for div_by_3

---
+++ Programming/test_div_by_3.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+import unittest
+import div_by_3
+
+class TestDivBy3(unittest.TestCase):
+
+    def test_divisble_by_3(self):
+        self.assertEqual(div_by_3.divisible_by_3(15), "aunty")
+        self.assertEqual(div_by_3.divisible_by_3(5), "uncle")
+        self.assertEqual(div_by_3.divisible_by_3(15.5), "uncle")
+        self.assertEqual(div_by_3.divisible_by_3(0), "aunty")
+
+
+if __name__ == "__main__":
+    unittest.main()
