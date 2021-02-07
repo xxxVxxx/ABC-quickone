d9baa67fb067215ec92c8e53d377d5548ada9bd8
Madhav <maddy4chat@gmail.com>
2021-02-08 08:38:35 +1100

    Created the program for division by 3

---
+++ Programming/div_by_3.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+import sys
+
+def divisible_by_3(number):
+    if not isinstance(number, (int):
+        raise TypeError('need an integer')
+    if (abs(number)%3 == 0):
+        return str("aunty")
+    else:
+        return str("uncle")
+
+
+if __name__ == "__main__":
+    try:
+        #Keeping it simple in the main func call to only make sure the input is integer
+        num = int(input("Enter your whole number: "))
+    except ValueError as e:
+        raise ValueError('only Integers are allowed.')
+    print(divisible_by_3(num))
