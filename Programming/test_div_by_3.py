45d922ac48cd0c24be03d75c3e832322be8fb463
Madhav <maddy4chat@gmail.com>
2021-02-08 08:49:56 +1100

    adding in test for Error checking for non integer

--- Programming/test_div_by_3.py
+++ Programming/test_div_by_3.py
@@ -10,7 +10,9 @@ class TestDivBy3(unittest.TestCase):
         self.assertEqual(div_by_3.divisible_by_3(5), "uncle")
         self.assertEqual(div_by_3.divisible_by_3(15.5), "uncle")
         self.assertEqual(div_by_3.divisible_by_3(0), "aunty")
-
+        
+        with self.assertRaises(TypeError):
+            div_by_3.divisible_by_3("somestring")
 
 if __name__ == "__main__":
     unittest.main()
